20f943b69282d32e69f09b129e958c961fd4c3c7
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-26 20:28:10 -0300

    Extension method with reduce

--- app/src/main/kotlin/br/com/rddev/array/SalarioComBigDecimal.kt
+++ app/src/main/kotlin/br/com/rddev/array/SalarioComBigDecimal.kt
@@ -12,18 +12,27 @@ fun main() {
 
     val arrayOfSalarios = bigDecimalArrayOf("1500.30", "2000.00", "3780.55", "5000", "10000")
     println(arrayOfSalarios.contentToString())
+    println("Gasto total sem aumento ${arrayOfSalarios.somatoria()}")
 
     val aumento = "1.1".toBigDecimal()
     val salariosReajustados = arrayOfSalarios
-        .map { salario ->
-            if (salario < "5000".toBigDecimal()) {
-                salario + "500".toBigDecimal()
-            } else {
-                (salario * aumento).setScale(2, RoundingMode.UP)
-            }
-        }
+        .map { salario -> calculaAumentoRelativo(salario, aumento) }
         .toTypedArray()
     println(salariosReajustados.contentToString())
+
+    println("soma de um array de inteiros ${intArrayOf(1,2,3).sum()}")
+    val gastoInicial = salariosReajustados.somatoria()
+    println("Gasto total com aumento $gastoInicial")
+
+}
+
+private fun calculaAumentoRelativo(
+    salario: BigDecimal,
+    aumento: BigDecimal
+) = if (salario < "5000".toBigDecimal()) {
+    salario + "500".toBigDecimal()
+} else {
+    (salario * aumento).setScale(2, RoundingMode.UP)
 }
 
 fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal>{
@@ -31,4 +40,10 @@ fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal>{
     return Array<BigDecimal>(valores.size) { i ->
         valores[i].toBigDecimal()
     }
+}
+
+fun Array<BigDecimal>.somatoria(): BigDecimal {
+    return this.reduce { acumulador, valor ->
+        acumulador + valor
+    }.setScale(2, RoundingMode.UP)
 }
