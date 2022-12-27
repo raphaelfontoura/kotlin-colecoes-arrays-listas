5f3d1630a616984cc279a31cb509f08b372cdcc3
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-26 22:44:31 -0300

    List Livros

---
+++ app/src/main/kotlin/br/com/rddev/list/Livro.kt
@@ -0,0 +1,13 @@
+package br.com.rddev.list
+
+data class Livro(
+    val titulo: String,
+    val autor: String,
+    val anoPublicacao: Long,
+    val editora: String? = null
+) : Comparable<Livro> {
+    override fun compareTo(other: Livro): Int {
+        return this.anoPublicacao.compareTo(other.anoPublicacao)
+    }
+
+}
