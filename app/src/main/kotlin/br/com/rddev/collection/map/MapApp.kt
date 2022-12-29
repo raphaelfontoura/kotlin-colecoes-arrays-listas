70c73fd5399514fba197a843b7a3800d02f2e706
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-29 17:20:23 -0300

    Test map with groupBy

--- app/src/main/kotlin/br/com/rddev/collection/map/MapApp.kt
+++ app/src/main/kotlin/br/com/rddev/collection/map/MapApp.kt
@@ -6,7 +6,8 @@ fun main() {
         Pedido(1, 20.0),
         Pedido(2, 60.0),
         Pedido(3, 30.0),
-        Pedido(4, 70.0)
+        Pedido(4, 70.0),
+        Pedido(5, 40.0)
     )
     println(pedidos)
 //    val pedidosMapeados = pedidos.associate { pedido: Pedido ->
@@ -21,6 +22,19 @@ fun main() {
     }
     println(pedidosFreteGratis)
     println(pedidosFreteGratis[Pedido(1, 20.0)])
+
+    val mapa = pedidos.associateBy { pedido -> pedido.valor > 50.0 }
+    // retornará apenas os últimos válidos, uma vez que só é possível duas chaves (true e false)
+    println(mapa)
+
+    val mapaFreteGratis = pedidos.groupBy { pedido -> pedido.valor > 50.0 }
+    println(mapaFreteGratis)
+    println(mapaFreteGratis[true])
+
+    val nomes = listOf("Alex", "Fran", "Gui", "Ana", "Paulo", "Maria", "Mario", "Gisele")
+    val agenda = nomes.groupBy { nome -> nome.first() }
+    println(agenda)
+    println(agenda['M'])
 
 }
 
