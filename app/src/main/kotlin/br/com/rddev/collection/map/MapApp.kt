a5784c15733b534d2543393be7a887a8c4be944a
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-29 17:09:24 -0300

    test map associate, associateBy and associateWith

--- app/src/main/kotlin/br/com/rddev/collection/map/MapApp.kt
+++ app/src/main/kotlin/br/com/rddev/collection/map/MapApp.kt
@@ -1,7 +1,27 @@
 package br.com.rddev.collection.map
 
 fun main() {
-    testaComportamentosMap()
+
+    val pedidos = listOf(
+        Pedido(1, 20.0),
+        Pedido(2, 60.0),
+        Pedido(3, 30.0),
+        Pedido(4, 70.0)
+    )
+    println(pedidos)
+//    val pedidosMapeados = pedidos.associate { pedido: Pedido ->
+//        Pair(pedido.numero, pedido) // pedido.numero to pedido
+//    }
+    val pedidosMapeados = pedidos.associateBy { pedido -> pedido.numero }
+    println(pedidosMapeados)
+    println(pedidosMapeados[1])
+
+    val pedidosFreteGratis = pedidos.associateWith { pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratis)
+    println(pedidosFreteGratis[Pedido(1, 20.0)])
 
 }
 
+data class Pedido(val numero:Int, val valor: Double)
